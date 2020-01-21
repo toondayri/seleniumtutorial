df85d6c7e4840ba0b4c4c35e21754c46d61bf039
tundeoduniyi <tunde.oduniyi@river-island.com>
2020-01-21 15:54:45 +0000

    added new tests

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,16 +1,19 @@
 package base;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
-import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
+    protected HomePage homePage;
 
+
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
 
@@ -19,32 +22,11 @@ public class BaseTests {
         System.out.println(driver.getTitle());
         driver.manage().window().setSize(new Dimension(375,812));
 
-
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println(links.size());
-
-
-        WebElement shiftingContentLink = driver.findElement(By.linkText("Shifting Content"));
-        shiftingContentLink.click();
-
-        WebElement menuElementLink =  driver.findElement(By.linkText("Example 1: Menu Element"));
-        menuElementLink.click();
-
-
-        List<WebElement> shiftingMenu = driver.findElements(By.cssSelector("div ul li"));
-        System.out.println(shiftingMenu.size());
-
-
-
-        //WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
-        //inputsLink.click();
-
-        driver.quit();
+        homePage = new HomePage(driver);
     }
 
-    public static void main(String[] args) {
-        BaseTests test = new BaseTests();
-        test.setUp();
-
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
     }
 }
