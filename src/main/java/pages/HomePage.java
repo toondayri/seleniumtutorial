f2c8f861c87a9b3fca22a3f9343041f69f54cd44
tundeoduniyi <tunde.oduniyi@river-island.com>
2020-01-22 10:11:31 +0000

    Updated dropdown tests

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,16 +6,27 @@ import org.openqa.selenium.WebDriver;
 public class HomePage {
 
     private WebDriver driver;
-    private By formAuthenticationLink = By.linkText("Form Authentication");
 
-    public HomePage(WebDriver driver){
+
+
+
+    public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
 
-    public LoginPage clickFormAuthentication(){
-        driver.findElement(formAuthenticationLink).click();
+    public LoginPage clickFormAuthentication() {
+       clickLink("Form Authentication");
         return new LoginPage(driver);
     }
 
+    public DropdownPage clickDropdown(){
+        clickLink("Dropdown");
+        return new DropdownPage(driver);
+    }
+
+
+    private void clickLink(String linkText){
+        driver.findElement(By.linkText(linkText)).click();
+    }
 }
